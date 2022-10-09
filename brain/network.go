d87d3bb06581d668fb149940da6df0a87e170c4e
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-10-09 00:24:12 -0500

    Update network.go

--- brain/network.go
+++ brain/network.go
@@ -1,4 +1,20 @@
 package brain
 
 type Network struct {
+	ConvLayers []Conv
+	SoftLayers []SoftMax
+	Pooling    []int
+}
+
+func NewNetwork(inputHeight, inputWidth, filterNum, widthAnHeightFilter, layersConv, output int, activationFunc []string, scaling []int) (net Network) {
+	net.Pooling = scaling
+	input := inputHeight * inputWidth
+	for i := 0; i < layersConv; i++ {
+		net.ConvLayers = append(net.ConvLayers, NewConv(filterNum, widthAnHeightFilter, "relu"))
+		input = (input * filterNum) / scaling[i]
+	}
+	for i := 0; i < len(activationFunc); i++ {
+		net.SoftLayers = append(net.SoftLayers, NewSoftMaxLayer(input, output))
+	}
+	return
 }
